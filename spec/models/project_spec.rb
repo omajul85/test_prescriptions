0e174bb934d1e2e28d4979ad1924d12e801ba865
Omar Alvarez <omajul2@hotmail.com>
2019-01-21 20:26:47 +0000

    Added first outside (the Rails stack) test to define functionality.

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -8,10 +8,9 @@
 #---
 require "rails_helper"
 
-RSpec.describe Project do 
+RSpec.describe Project do
   it "considers a project with no tasks to be done" do
     project = Project.new
-    expect(project.done?).to be_truthy 
+    expect(project.done?).to be_truthy
   end
-
 end
