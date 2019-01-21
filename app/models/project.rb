42b27fbf8c0dc61c50dadf2513c876b32dd36fd5
Omar Alvarez <omajul2@hotmail.com>
2019-01-21 20:42:41 +0000

    Added first 2 model tests for project using predicate methods.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,13 +1,7 @@
-#---
-# Excerpted from "Rails 5 Test Prescriptions",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material,
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose.
-# Visit http://www.pragmaticprogrammer.com/titles/nrtest3 for more book information.
-#---
-class Project
+class Project < ApplicationRecord
+  has_many :tasks, dependent: :destroy
+
   def done?
-    true
+    tasks.empty?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@
 # Visit http://www.pragmaticprogrammer.com/titles/nrtest3 for more book information.
 #---
 Rails.application.routes.draw do
+  resources :tasks
+  resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
