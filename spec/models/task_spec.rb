f8d27a7e5f64ead3ab1820c931e28b84cc3ed191
Omar Alvarez <omajul2@hotmail.com>
2019-01-22 21:10:56 +0000

    Added tests for velocity functionality (handle datetime).

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -6,4 +6,25 @@ RSpec.describe Task, type: :model do
   it 'does not have a new task as complete' do
     expect(task).not_to be_complete
   end
+
+  describe 'velocity' do
+    let(:task) { Task.new(size: 3) }
+
+    it 'does not count an incomplete task towards velocity' do
+      expect(task).not_to be_a_part_of_velocity
+      expect(task.points_toward_velocity).to eq(0)
+    end
+
+    it 'counts a recently complete task towards velocity' do
+      task.mark_completed(1.day.ago)
+      expect(task).to be_a_part_of_velocity
+      expect(task.points_toward_velocity).to eq(3)
+    end
+
+    it 'does not count a long-ago completed task towards velocity' do
+      task.mark_completed(5.weeks.ago)
+      expect(task).not_to be_a_part_of_velocity
+      expect(task.points_toward_velocity).to eq(0)
+    end
+  end
 end
