806118d8ea5d3220b9f1d50e3fefb02c3cd62481
Omar Alvarez <omajul2@hotmail.com>
2019-01-23 20:55:13 +0000

    Added test using the Time data.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,17 +1,26 @@
 class Task #< ApplicationRecord
   # belongs_to :project
-  attr_accessor :size, :completed
+  attr_accessor :size, :completed_at
 
   def initialize(opts = {})
     @size = opts[:size]
-    @completed = opts[:completed]
+    @completed_at = opts[:completed_at] if opts[:completed_at]
   end
 
-  def mark_completed
-    @completed = true
+  def mark_completed(date: Time.current)
+    @completed_at = date
   end
 
   def complete?
-    @completed
+    completed_at.present?
+  end
+
+  def part_of_velocity?
+    return false unless complete?
+    completed_at > 21.days.ago
+  end
+
+  def points_toward_velocity
+    part_of_velocity? ? size : 0
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -16,13 +16,13 @@ RSpec.describe Task, type: :model do
     end
 
     it 'counts a recently complete task towards velocity' do
-      task.mark_completed(1.day.ago)
+      task.mark_completed(date: 1.day.ago)
       expect(task).to be_a_part_of_velocity
       expect(task.points_toward_velocity).to eq(3)
     end
 
     it 'does not count a long-ago completed task towards velocity' do
-      task.mark_completed(5.weeks.ago)
+      task.mark_completed(date: 5.weeks.ago)
       expect(task).not_to be_a_part_of_velocity
       expect(task.points_toward_velocity).to eq(0)
     end
