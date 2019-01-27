ea8c3a6e96fd6a47b33e03592f1f918bdc688c9f
Omar Alvarez <omajul2@hotmail.com>
2019-01-27 15:36:39 +0000

    Added test for handling empty Strings.

--- app/workflows/creates_project.rb
+++ app/workflows/creates_project.rb
@@ -1,11 +1,26 @@
 class CreatesProject
-  attr_accessor :name, :project
+  attr_accessor :name, :project, :task_string
 
-  def initialize(name: '')
+  def initialize(name: '', task_string: '')
     @name = name
+    @task_string = task_string
   end
 
   def build
     @project = Project.new(name: name)
+  end
+
+  def convert_string_to_tasks
+    task_string.split('\n').map do |task|
+      title, size_string = task.split(':')
+      Task.new(title: title, size: size_as_integer(size_string))
+    end
+  end
+
+  private
+
+  def size_as_integer(size_string)
+    return 1 if size_string.blank?
+    [size_string.to_i, 1].max
   end
 end
--- spec/workflows/creates_project_spec.rb
+++ spec/workflows/creates_project_spec.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe CreatesProject do
-  it 'creates a project given a name' do
-    creator = CreatesProject.new(name: 'Project Runaway')
-    creator.build
-    expect(creator.project.name).to eq('Project Runaway')
+  let(:creator) { CreatesProject.new(name: 'Project Runaway', task_string: task_string) }
+
+  describe 'initialization' do
+    let(:task_string) { '' }
+    it 'creates a project given a name' do
+      creator.build
+      expect(creator.project.name).to eq('Project Runaway')
+    end
+  end
+
+  describe 'task String parsing' do
+    let(:tasks) { creator.convert_string_to_tasks }
+
+    describe 'with an empty string' do
+      let(:task_string) { '' }
+      specify { expect(tasks).to be_empty }
+    end
   end
 end
