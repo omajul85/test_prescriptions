fd55070a17b2949a931f5a194183e96e23b5fd53
Omar Alvarez <omajul2@hotmail.com>
2019-02-17 16:45:17 +0000

    Used spies to simulate DB failure

--- spec/system/add_project_spec.rb
+++ spec/system/add_project_spec.rb
@@ -19,4 +19,16 @@ RSpec.describe 'adding a project', type: :system do
     click_on 'Create Project'
     expect(page).to have_selector('.new_project')
   end
+
+  it 'behaves correctly in the event of a surprising DB failure' do
+    workflow = instance_spy(CreatesProject, success?: false, project: Project.new)
+    allow(CreatesProject).to receive(:new)
+      .with(name: 'Real Name', task_string: "Choose fabric:3\r\nMake it work:5")
+      .and_return(workflow)
+    visit new_project_path
+    fill_in 'Name', with: 'Real Name'
+    fill_in 'project[:tasks]', with: "Choose fabric:3\nMake it work:5"
+    click_on 'Create Project'
+    expect(page).to have_selector('.new_project')
+  end
 end
--- spec/workflows/creates_project_spec.rb
+++ spec/workflows/creates_project_spec.rb
@@ -77,5 +77,15 @@ RSpec.describe CreatesProject do
         expect(creator).not_to be_a_success
       end
     end
+
+    describe 'mocking a failure' do
+      it 'fails when we say it fails' do
+        project = instance_spy(Project, save: false)
+        allow(Project).to receive(:new).and_return(project)
+        creator = CreatesProject.new(name: 'Name', task_string: "Task")
+        creator.create
+        expect(creator).not_to be_a_success
+      end
+    end
   end
 end
