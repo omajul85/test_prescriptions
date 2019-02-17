9a8cc78b4510518b25304ca2161449fb77ab9ea4
Omar Alvarez <omajul2@hotmail.com>
2019-02-17 18:13:14 +0000

    Used mocks to specify behaviour

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
+  it 'calls the workflow with parameters' do
+    workflow = instance_spy(CreatesProject, success?: true)
+    allow(CreatesProject).to receive(:new).and_return(workflow)
 
+    post :create, params: {project: { "name" => 'Runaway', ":tasks" => "start something:2"}}
+    expect(CreatesProject).to have_received(:new)
+      .with(name: 'Runaway', task_string: "start something:2")
+  end
 end
